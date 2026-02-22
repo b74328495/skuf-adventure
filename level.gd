1637e792a23ef90454801599087143844842bd33
Boris <b18705151@gmail.com>
2026-02-22 19:05:40 +0300

    Обновил версию проекта.

--- level.gd
+++ level.gd
@@ -9,24 +9,39 @@ enum {
 
 var state = MORNING
 
-func _process(delta):
+func _ready():
+	change_light()
+
+func morning_state():
+	var tween = get_tree().create_tween()
+	tween.tween_property(light, "energy", 0.5, 20)
+	
+func day_state():
+	var tween = get_tree().create_tween()
+	tween.tween_property(light, "energy", 1.0, 20)
+	
+func evening_state():
+	var tween = get_tree().create_tween()
+	tween.tween_property(light, "energy", 0.5, 20)
+	
+func night_state():
+	var tween = get_tree().create_tween()
+	tween.tween_property(light, "energy", 0.2, 20)
+
+func change_light():
 	match state:
 		MORNING:
 			morning_state()
+		DAY:
+			day_state()
 		EVENING:
 			evening_state()
-			
-
-func evening_state ():
-	var tween = get_tree().create.tween()
-	tween.tween_property(light, "energy", 0.2, 20)
-func evening_state ():
-	var tween = get_tree().create.tween()
-	tween.tween_property(light, "energy", 0.95, 20)
-	
+		NIGHT:
+			night_state()
 	
 func _on_day_night_timeout():
 	if state < 3:
 		state += 1
 	else:
 		state = MORNING
+	change_light()  # Вызываем смену освещения при смене состояния	\1Ч
